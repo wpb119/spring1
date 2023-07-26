4d82955dd6b20126f3d6cff0430166deac92a686
wpb119 <wpb119@gmail.com>
2023-07-26 06:41:51 -0500

    2nd commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class DemoApplication {
-
+    @Value("${spring.application.name}")
+    private String name;
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
     @RequestMapping(value = "/")
-    public String hello() {
-        return "Hello World";
+    public String name() {
+        return name;
     }
 }
